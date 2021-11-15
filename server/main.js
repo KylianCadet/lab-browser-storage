af19df2464b83edf9d06bb84b245fa5964bdc595
Kylian <kylian.cadet@epitech.eu>
2021-11-16 00:50:51 +0100

    add cookie + sessionStorage + indexedDB

--- server/main.js
+++ server/main.js
@@ -2,25 +2,41 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const port = process.env.PORT ? process.env.PORT : 3000;
+const allowedOrigins = [
+  "http://localhost:8000",
+  "https://kyliancadet.github.io/",
+];
+const corsOptions = {
+  // origin: function (origin, callback) {
+  //   if (allowedOrigins.indexOf(origin) !== -1) return callback(null, true);
+  //   callback(new Error("Not allowed by CORS"));
+  // },
+  origin: true,
+  credentials: true,
+};
+
 const app = express();
 app.use(cookieParser());
-app.use(cors({
-  origin: '*'
-}));
+app.use(cors(corsOptions));
 
-const port = process.env.PORT ? process.env.PORT : 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/cookie/get", (req, res) => {
+  console.log(req.cookies);
+  res.send("test");
 });
 
 app.get("/cookie", (req, res) => {
-  const cookieVal = req.params["cookie"] ? req.params["cookie"] : "foo";
+  console.log(req.cookies)
+  const cookieVal = req.query["cookie"] ? req.query["cookie"] : "foo";
   res.cookie("ThisIsMyCookie", cookieVal, {
-    maxAge: 900000,
-    httpOnly: true,
+    expires: req.query["exprires"] ? req.query["exprires"] : null,
+    secure: req.query["secure"] === "true" ? true: false,
+    httpOnly: false,
+    domain: req.query["domain"] ? req.query["domain"] : null,
+    path: req.query["path"] ? req.query["path"] : null,
+    sameSite: req.query["sameSite"] ? req.query["sameSite"] : null,
   });
-  res.status(200).send("OK");
+  res.status(200).send(req.cookies);
 });
 
 app.listen(port, () => {
