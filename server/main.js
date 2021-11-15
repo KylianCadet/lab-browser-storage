770ebee9bf9e33722bbfc4d0ff2374ce7aec5eab
Kylian <kylian.cadet@epitech.eu>
2021-11-15 20:46:57 +0100

    add cookie in server, modify client 	modified:   client/cookies/index.html 	modified:   server/main.js

--- server/main.js
+++ server/main.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+app.use(express.cookieParser());
 const port = process.env.PORT ? process.env.PORT: 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/cookie', (req, res) => {
+  const cookieVal = req.params['cookie'] ? req.params['cookie'] : 'foo'
+  res.cookie('ThisIsMyCookie', randomNumber, { maxAge: 900000, httpOnly: true });
+  res.send('Hello World!')
+})
+
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
